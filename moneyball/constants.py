7470f04ae05258ca5b6bda1b4e1bc00f4ab83843
jchick2 <111472161+jchick2@users.noreply.github.com>
2022-12-11 18:53:40 -0500

    Update constants.py

--- moneyball/constants.py
+++ moneyball/constants.py
@@ -11,7 +11,7 @@ METRIC_PATH = {
 }
 
 # MLB available seasons for data scraping
-MIN_YEAR = 2000
+MIN_YEAR = 1990
 MAX_YEAR = (
     datetime.today().year if datetime.today().month >= 4 else datetime.today().year - 1
 )  # Current year only if regular season is ongoing/finished
